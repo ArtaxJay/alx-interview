234d7af8ec73e407b831ba75e029a5785bba633d
Ola-Oluwajuwon <okayode986@stu.ui.edu.ng>
2024-11-24 12:42:53 -0800

    Feature: first attempt at writing a function that can rotate a 2D matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,30 @@
+
+#!/usr/bin/python3
+'''
+This module contains a function that rotates a 2D matrix 90° clockwise
+'''
+
+
+def rotate_2d_matrix(matrix):
+    '''
+    This function rotates a 2D matrix 90° clockwise
+    Returns: Null
+    '''
+    to_left = 0
+    to_right = len(matrix) - 1
+
+    while to_left < to_right:
+        for i in range(to_right - to_left):
+            to_top, to_bottom = to_left, to_right
+            # save topleft  value
+            to_top_left = matrix[to_top][to_left + i]
+            # move bottom left to top left
+            matrix[to_top][to_left + i] = matrix[to_bottom - i][to_left]
+            # move bottom right to bottom left
+            matrix[to_bottom - i][to_left] = matrix[to_bottom][to_right - i]
+            # move top right to bottom right
+            matrix[bottom][to_right - i] = matrix[top + i][to_right]
+            # move top left to top right
+            matrix[to_top + i][to_right] = to_top_left
+        to_right -= 1
+        to_left += 1
---
+++ 0x07-rotate_2d_matrix/README.md
@@ -0,0 +1,9 @@
+# 0x07. Rotate 2D Matrix
+## Learning Tags:
+- Algorithm
+- Python
+
+# This project is about rotating a 2D matrix.
+# And was done as part of the ALX Software Engineering program.
+**Author:**
+- [Ola-Oluwajuwon Kayode](https://github.com/Ola-Oluwajuwon)
