5522512a4957720458a15c83601486c86321961d
Ola-Oluwajuwon <okayode986@stu.ui.edu.ng>
2024-11-24 13:07:50 -0800

    Fix: another attempt to make the function work as expected.

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,33 +1,40 @@
-
-#!/usr/bin/python3
-'''
-This module contains a function that rotates a 2D matrix 90° clockwise
-'''
+"""
+This module provides a function to rotate a 2D matrix 90 degrees clockwise.
+"""
 
 
 def rotate_2d_matrix(matrix):
-    '''
-    This function rotates a 2D matrix 90° clockwise
-    Returns: Null
-    '''
-    to_left = 0
-    to_right = len(matrix) - 1
+    """
+    Rotates a given 2D matrix 90 degrees clockwise in-place.
 
-    while to_left < to_right:
-        for i in range(to_right - to_left):
-            to_top, to_bottom = to_left, to_right
-            # save topleft  value
-            to_top_left = matrix[to_top][to_left + i]
-            # move bottom left to top left
-            matrix[to_top][to_left + i] =
-                matrix[to_bottom - i][to_left]
-            # move bottom right to bottom left
-            matrix[to_bottom - i][to_left] =
-                matrix[to_bottom][to_right - i]
-            # move bottom right to bottom left
-            matrix[to_bottom][to_right - i] =
-            matrix[to_top + i][to_right]
-            # move top left to top right
-            matrix[to_top + i][to_right] = to_top_left
-        to_right -= 1
-        to_left += 1
+    Parameters:
+        matrix (list of list of int): The 2D matrix to be rotated.
+
+    Returns:
+        None: The matrix is modified in-place.
+    """
+    left = 0
+    right = len(matrix) - 1
+
+    while left < right:
+        for offset in range(right - left):
+            top = left
+            bottom = right
+
+            # Save the top-left value
+            top_left = matrix[top][left + offset]
+
+            # Move bottom-left to top-left
+            matrix[top][left + offset] = matrix[bottom - offset][left]
+
+            # Move bottom-right to bottom-left
+            matrix[bottom - offset][left] = matrix[bottom][right - offset]
+
+            # Move top-right to bottom-right
+            matrix[bottom][right - offset] = matrix[top + offset][right]
+
+            # Move top-left to top-right
+            matrix[top + offset][right] = top_left
+
+        left += 1
+        right -= 1
